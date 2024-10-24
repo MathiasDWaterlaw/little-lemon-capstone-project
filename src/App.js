4f761b2a7499d883b918f18bed163e8fb4a5b4e5
MathiasDWaterlaw <mathiasdellosa@gmail.com>
2024-10-24 15:45:51 +0200

    project structure and assets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 import "./App.css";
 
 function App() {
-  return <div className='App'>Homepage</div>;
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <main>
+      <section id='specials'></section>
+      <section id='testimonials'></section>
+      <section id='about'></section>
+    </main>
+  );
+}
