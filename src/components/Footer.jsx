74b157785334adde37989271855f37386dfa50c3
MathiasDWaterlaw <mathiasdellosa@gmail.com>
2024-10-25 01:17:01 +0200

    added og protocol

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,54 @@
+import footerLogo from "../assets/logo/footer_logo.png";
+
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <img src={footerLogo} />
+
+      <ul id='doormat-navigation'>
+        <li>
+          <a href=''>Home</a>
+        </li>
+        <li>
+          <a href=''>About</a>
+        </li>
+        <li>
+          <a href=''>Menu</a>
+        </li>
+        <li>
+          <a href=''>Reservation</a>
+        </li>
+        <li>
+          <a href=''>Order Online</a>
+        </li>
+        <li>
+          <a href=''>Login</a>
+        </li>
+      </ul>
+
+      <ul id='Contact'>
+        <li>
+          <a href=''>Address</a>
+        </li>
+        <li>
+          <a href=''>Phone</a>
+        </li>
+        <li>
+          <a href=''>Email</a>
+        </li>
+      </ul>
+
+      <ul id='social-media'>
+        <li>
+          <a href=''>Facebook</a>
+        </li>
+        <li>
+          <a href=''>Instagram</a>
+        </li>
+        <li>
+          <a href=''>TikTok</a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,29 @@
+import logo from "../assets/logo/Logo.svg";
+
 export default function Nav() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <img src={logo} />
+      <ul>
+        <li id='home'>
+          <a href=''>HOME</a>
+        </li>
+        <li id='about'>
+          <a href=''>ABOUT</a>
+        </li>
+        <li id='menu'>
+          <a href=''>MENU</a>
+        </li>
+        <li id='reservation'>
+          <a href=''>RESERVATION</a>
+        </li>
+        <li id='order-online'>
+          <a href=''>ORDER ONLINE</a>
+        </li>
+        <li id='login'>
+          <a href=''>LOGIN</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
